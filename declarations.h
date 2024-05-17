a767110e2eb6a3d73b6d67ca505bcc3aeb6fccab
Ayush <162930903+coyfisha@users.noreply.github.com>
2024-05-17 09:21:09 -0400

    Create declarations.h

---
+++ declarations.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cstdlib> //for rand
+#include <ctime> //for time
+
+
+const std::string RED("\033[0;31m");
+const std::string GREEN("\033[1;32m");
+const std::string YELLOW("\033[1;33m");
+const std::string RESET("\033[0m");
+const std::string CLEAR("\033[2J\033[0;0H");
+//declarations for colours to make shortcuts
+
+
+std::string ranb_word();
